3aea92a84546487e51c26c288cba67f59617a3a2
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2021-01-22 21:44:59 +0300

    Update load_csv.py

--- load_csv.py
+++ load_csv.py
@@ -1,6 +1,7 @@
 import csv
 from pprint import pprint
-# for categories
+from app import db, Dish
+for categories
 with open('delivery_categories.csv', newline='', encoding='utf-8') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     date = []
@@ -9,18 +10,8 @@ with open('delivery_categories.csv', newline='', encoding='utf-8') as csvfile:
     for cat in date[1:]:
         db.session.add(Category(title=cat[0][2:]))
     db.session.commit()
-# for dishes. Or dishs?
-with open('delivery_items.csv', newline='', encoding='utf-8') as csvfile:
-    """
-    class Dish(db.Model):
-    __tablename__ = 'dishs'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    picture = db.Column(db.String, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
-    category = db.relationship('Category', back_populates='dishs')
-    """
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar=' ')
-    # i don't know
+with open('delivery_items.csv', encoding='utf-8') as csvfile:
+    delivery_items = csv.DictReader(csvfile)
+    for delivery_item in delivery_items:
+        db.session.add(Dish(**delivery_item))
+db.session.commit()
